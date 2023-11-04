3c4e505c21b2039f42a8d1381f4492387f659313
jonathan de alba <jdealba15@yahoo.com>
2023-11-04 13:35:24 -0700

    first commit

---
+++ call-stack/script.js
@@ -0,0 +1,40 @@
+// Call Stack, data structure that functions 'last in, first out...'
+// Manager for functions. 
+
+// Open 'sources' tab and put a breakpoint at the first() function
+
+// First Example
+
+function first() {
+  console.log('first...');
+}
+
+function second() {
+  console.log('second...');
+}
+
+function third() {
+  console.log('third...');
+}
+
+first();
+second();
+third();
+
+// Second Example
+
+// function first() {
+//   console.log('first...');
+//   second();
+// }
+
+// function second() {
+//   console.log('second...');
+//   third();
+// }
+
+// function third() {
+//   console.log('third...');
+// }
+
+// first();
