8af1aeead7cfc603262ea4b65aca61552bf734a8
jonathan de alba <jdealba15@yahoo.com>
2023-12-10 15:17:57 -0800

    first commit

--- try-catch/script.js
+++ try-catch/script.js
@@ -4,6 +4,9 @@
 //   console.log('Error: ' + error);
 // }
 
+// remember, "error" on line 3 is a placeholder term, for whatever data being passed through
+// "'Error'" on line 4 is a string, concatenated with our "error" placeholder term
+
 function double(number) {
   if (isNaN(number)) {
     throw new Error(number + ' is not a number');
@@ -13,7 +16,7 @@ function double(number) {
 }
 
 try {
-  const y = double('hello');
+  const y = double('3');
   console.log(y);
 } catch (error) {
   console.log(error);
