0e525f27a5b6e978955f6fe01c1897264c67c9b1
jonathan de alba <jdealba15@yahoo.com>
2024-02-12 10:35:39 -0800

    first commit

---
+++ for-loop/script.js
@@ -0,0 +1,30 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log('Number' + i);
+  
+//     for (let j = 1; j <= 11; j++) {
+//       console.log(`${i} + ${j} = ${i + j}`);
+//     }
+//   }
+
+// Testing for index
+// const names = ["Brad", "Sam", "Sara", "John", "Tim"];
+
+// for (let i = 0; i <= names.length; i++) {
+//     if(i === 2) {
+//         console.log(names[i] + " is the best");
+//     } else {
+//     console.log(names[i]);
+//     }
+// }
+
+// Testing for value
+const names = ['Brad', 'Sam', 'Sara', 'John', 'Tim'];
+
+    for (let i = 0; i <= names.length; i++) {
+        if (names[i] === "Brad") {
+            console.log(names[i] + " is the best");
+        } else {
+        console.log(names[i]);
+        }
+    }
+
