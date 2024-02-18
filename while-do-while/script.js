17f1d52d5751aa53595eae1e7d0526ac31e32f8d
jonathan de alba <jdealba15@yahoo.com>
2024-02-18 13:32:32 -0800

    first commit

---
+++ while-do-while/script.js
@@ -0,0 +1,25 @@
+let i = 1;
+
+// while (i <= 20) {
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+const arr = [10, 20, 30, 40];
+
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+while (i <= 5) {
+    console.log('Number ' + i);
+
+    let j = 1;
+    while (j <= 5) {
+        console.log(`${i} * ${j} = ${i * j}`);
+        j++;
+    }
+
+    i++
+}
