3926264d29a8ec816974c6dca359a645c894e7cc
jonathan de alba <jdealba15@yahoo.com>
2024-02-20 11:14:36 -0800

    first commit

--- music-player-project/script.js
+++ music-player-project/script.js
@@ -40,6 +40,20 @@ function pauseSong() {
 // The Element.classList is a read-only property that returns a live DOMTokenList collection of 
 // the class attributes of the element.
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+} 
+
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
   
@@ -49,3 +63,33 @@ playBtn.addEventListener('click', () => {
       playSong();
     }
   });
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length -1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
