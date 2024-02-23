8cf1bd1407cb3e90ee8ecca126dfaaf74cce9ab7
jonathan de alba <jdealba15@yahoo.com>
2024-02-23 08:32:02 -0800

    first commit

---
+++ fizzbuzz-challenge/script.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 100; i++) {
+    if( i % 5 == 0 && i % 3 == 0 ){
+        console.log("FizzBuzz");
+        continue;
+    }
+    else if( i % 3 == 0 ){
+        console.log("Fizz");
+        continue;
+    }
+    else if( i % 5 == 0 ){
+        console.log("Buzz");
+        continue;
+    }
+    console.log(i);
+  }
+
+  
