2ed59b209fd3646ad9c50323158e91e327bcc010
jonathan de alba <jdealba15@yahoo.com>
2024-03-02 14:09:21 -0800

    first commit

---
+++ nodemon-routes-cleanup/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
+//Get all ideas
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter)
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
