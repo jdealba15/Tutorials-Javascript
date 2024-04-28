db51b705ec0aec65af7ed76b445c0b63402c9520
Jonathan De Alba <jdealba15@yahoo.com>
2024-04-28 12:43:34 -0700

    first commit

---
+++ for-of-loop/script.js
@@ -0,0 +1,23 @@
+const items = ['phone', 'silverware', 'water-bottle', 'computer'];
+const users = [{ name: 'John' }, { name: 'Jorge'}, { name: 'Romulo'}, { name: 'Michael'}];
+
+for (const item of items) {
+    console.log(item);
+}
+
+for (const user of users) {
+    console.log(user.name);
+}
+
+const str = "Hello World";
+for(const letter of str) {
+    console.log(letter);
+}
+
+const method = new Map();
+method.set('name', 'John');
+method.set('age', 30);
+console.log(method);
+for (const [key, value] of method) {
+    console.log(key, value);
+}
